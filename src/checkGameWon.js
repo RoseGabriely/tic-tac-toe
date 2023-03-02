1b2c05852a6420c63aa88ab221df47bf6d9a6032
RoseGabriely <rosegabriely14@gmail.com>
2023-03-02 10:26:42 -0500

    Adds function that checks if game is won

---
+++ src/checkGameWon.js
@@ -0,0 +1,21 @@
+function checkGameWon(board) {
+  const combos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < combos.length; i++) {
+    const [a, b, c] = combos[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return true;
+    }
+  }
+}
+
+export default checkGameWon;
