5ac022efc769d6f63761c0611bd132ff8aea57e1
RoseGabriely <rosegabriely14@gmail.com>
2023-03-04 13:44:41 -0500

    Adds message state & checks for if a tile is taken

--- src/App.js
+++ src/App.js
@@ -4,35 +4,45 @@ import checkGameWon from "./checkGameWon";
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(""));
-  const [player, setPlayer] = useState("Player 1");
+  const [player, setPlayer] = useState("Player X");
+  const [message, setMessage] = useState("It's player X's turn!");
 
   function handleMove(index) {
-    if (player === "Player 1") {
+    if (board[index] !== "") {
+      setMessage("That space is taken!");
+    } else if (player === "Player X") {
       const addMove = [...board];
       addMove[index] = "X";
       setBoard(addMove);
-      checkGameWon(addMove)
-        ? setPlayer("Player 1 wins!")
-        : setPlayer("Player 2");
-    } else if (player === "Player 2") {
+      if (checkGameWon(addMove) === true) {
+        setMessage("Player X wins!");
+      } else {
+        setPlayer("Player O");
+        setMessage("It's player O's turn!");
+      }
+    } else if (player === "Player O") {
       const addMove = [...board];
       addMove[index] = "O";
       setBoard(addMove);
-      checkGameWon(addMove)
-        ? setPlayer("Player 2 wins!")
-        : setPlayer("Player 1");
+      if (checkGameWon(addMove) === true) {
+        setMessage("Player O wins!");
+      } else {
+        setPlayer("Player X");
+        setMessage("It's player X's turn!");
+      }
     }
   }
 
   function handleRestart() {
     setBoard(Array(9).fill(""));
-    setPlayer("Player 1");
+    setPlayer("Player X");
+    setMessage("It's player X's turn!");
   }
 
   return (
     <div className="App">
       <h1>Tic-Tac-Toe</h1>
-      <h2>{checkGameWon(board) ? `${player}` : `It's ${player}'s turn!`}</h2>
+      <h2>{message}</h2>
       <div className="Board">
         {board.map((buttonText, index) => (
           <button
