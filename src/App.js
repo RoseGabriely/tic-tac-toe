50d2c76f6b822d015cbf9d8c4cd9f7c605965bf4
RoseGabriely <rosegabriely14@gmail.com>
2023-03-04 15:41:43 -0500

    Creates separate function for updateBoard

--- src/App.js
+++ src/App.js
@@ -4,38 +4,37 @@ import checkGameWon from "./checkGameWon";
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(""));
-  const [player, setPlayer] = useState("Player X");
+  const [player, setPlayer] = useState("X");
   const [message, setMessage] = useState("It's player X's turn!");
 
+  function updateBoard(index) {
+    const addMove = [...board];
+    addMove[index] = player;
+    setBoard(addMove);
+    if (checkGameWon(addMove) === true) {
+      setMessage(`Player ${player} wins!`);
+    } else {
+      player === "X" ? setPlayer("O") : setPlayer("X");
+      player === "X"
+        ? setMessage("It's player O's turn!")
+        : setMessage("It's player X's turn!");
+    }
+  }
+
   function handleMove(index) {
+    console.log(checkGameWon(board));
     if (board[index] !== "") {
       setMessage("That space is taken!");
-    } else if (player === "Player X") {
-      const addMove = [...board];
-      addMove[index] = "X";
-      setBoard(addMove);
-      if (checkGameWon(addMove) === true) {
-        setMessage("Player X wins!");
-      } else {
-        setPlayer("Player O");
-        setMessage("It's player O's turn!");
-      }
-    } else if (player === "Player O") {
-      const addMove = [...board];
-      addMove[index] = "O";
-      setBoard(addMove);
-      if (checkGameWon(addMove) === true) {
-        setMessage("Player O wins!");
-      } else {
-        setPlayer("Player X");
-        setMessage("It's player X's turn!");
-      }
+    } else if (player === "X") {
+      updateBoard(index);
+    } else if (player === "O") {
+      updateBoard(index);
     }
   }
 
   function handleRestart() {
     setBoard(Array(9).fill(""));
-    setPlayer("Player X");
+    setPlayer("X");
     setMessage("It's player X's turn!");
   }
 
